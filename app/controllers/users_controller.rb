488bd14bc1cd06c7b16f76c4d83d5250a17031d1
David Cliff <david.g.cliff@gmail.com>
2023-04-25 19:52:04 +0000

    Rubocop clean

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@
 
 # Users
 class UsersController < ApplicationController
-  api :POST, "/users", "Create a user"
-  param :name, String, "Name of the user"
-  param :nuid, String, "NEU ID"
-  returns :code => 200, :desc => "a successful response" do
-    property :id, String, :desc => "User ID"
- end
+  api :POST, '/users', 'Create a user'
+  param :name, String, 'Name of the user'
+  param :nuid, String, 'NEU ID'
+  returns code: 200, desc: 'a successful response' do
+    property :id, String, desc: 'User ID'
+  end
   def create
     # TODO: restrict to admin user
     u = User.create(user_params)
-    render json: {'id' => u.id}.to_json
+    render json: { 'id' => u.id }.to_json
   end
 
   api :GET, '/users/:id'
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :nuid, :email).merge(password: Devise.friendly_token[0,20])
+    params.require(:user).permit(:name, :nuid, :email).merge(password: Devise.friendly_token[0, 20])
   end
 end

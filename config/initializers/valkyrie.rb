61742bf53891b30cc4ca35784950d4da9bf70891
David Cliff <david.g.cliff@gmail.com>
2023-04-28 15:59:58 +0000

    Stubbing out valkyrie yml and short versions of long calls

--- config/initializers/valkyrie.rb
+++ config/initializers/valkyrie.rb
@@ -2,3 +2,11 @@ Valkyrie::MetadataAdapter.register(
   Valkyrie::Persistence::Postgres::MetadataAdapter.new,
   :postgres
 )
+
+def self.persister
+  Valkyrie.config.metadata_adapter.persister
+end
+
+def self.query_service
+  Valkyrie.config.metadata_adapter.query_service
+end

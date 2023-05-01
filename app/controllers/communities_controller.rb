116170aaa456f027ae784d56e09a35d50a6c066c
David Cliff <david.g.cliff@gmail.com>
2023-05-01 17:11:25 +0000

    Stubbing out first valkyrie CRUD, and adding in decorators

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -4,7 +4,9 @@
 class CommunitiesController < ApplicationController
   def index; end
   def show; end
-  def create; end
+  def create
+    community = CommunityCreator.call()
+  end
   def update; end
   def destroy; end
 end

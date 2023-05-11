203eeb21835940788f4217503dd60e52d9e05fec
David Cliff <david.g.cliff@gmail.com>
2023-05-11 16:40:50 +0000

    First pass at work controller copy pasta

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,9 +2,27 @@
 
 # Works
 class WorksController < ApplicationController
-  def index; end
-  def show; end
-  def create; end
-  def update; end
-  def destroy; end
+  include LazyPagination
+
+  def index
+    @pagination, @works = paginate_model(Work)
+  end
+  def show
+    @work = Work.find(params[:id]).decorate
+  end
+  def create
+    # TODO: XML
+    work = WorkCreator.call()
+  end
+  def mods
+    @work = Work.find(params[:id]).decorate
+  end
+  def update
+    work = Work.find(params[:id])
+    work.mods_xml = parse_xml
+  end
+  def destroy
+    # TODO: restrict to admin user
+    Atlas.persister.delete(resource: Work.find(params[:id]))
+  end
 end

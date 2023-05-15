5d4240ceac72056cdc2e99047959b3b010e2f86d
David Cliff <david.g.cliff@gmail.com>
2023-05-15 18:43:25 +0000

    Stubbing out atlas binary versioning in blobs controller

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -3,6 +3,7 @@
 # Blobs
 class BlobsController < ApplicationController
   include LazyPagination
+  include FileHelper
 
   def index
     @pagination, @blobs = paginate_model(Blob)
@@ -27,6 +28,11 @@ class BlobsController < ApplicationController
 
   def update
     blob = Blob.find(params[:id])
+    file = params[:binary]
+    path = file.tempfile.path.presence || file.path
+    file_id = create_file(path, blob).id
+    blob.file_identifiers += [file_id]
+    Atlas.persister.save(resource: blob)
   end
 
   def destroy

55ce2512fa01e0ff783aa265d10be0e1b3a2f1ef
David Cliff <david.g.cliff@gmail.com>
2023-05-17 18:51:23 +0000

    render views for jbuilder and parsing json - still need to load in some fixture mods xml

--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -4,18 +4,17 @@ require 'rails_helper'
 
 RSpec.describe CommunitiesController, type: :controller do
   describe 'GET #show' do
+    render_views
+
     context 'when the community exists' do
       let(:community) { CommunityCreator.call }
 
       it 'returns the community details' do
-        expect(community.noid).to be_a String
-        # get :show, params: { id: user.id }
-        # expect(response).to have_http_status(:success)
+        get :show, params: { id: community.noid }, as: :json
+        expect(response).to have_http_status(:success)
 
-        # json_response = JSON.parse(response.body)
-        # expect(json_response['id']).to eq(user.id)
-        # expect(json_response['name']).to eq(user.name)
-        # expect(json_response['email']).to eq(user.email)
+        json_response = JSON.parse(response.body)
+        expect(json_response['community']['id']).to eq(community.noid)
       end
     end
   end

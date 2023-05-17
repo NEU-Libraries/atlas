db4cd3687e7d91243206c3f19ea638aa4a1d42ff
David Cliff <david.g.cliff@gmail.com>
2023-05-17 17:59:31 +0000

    Test persistence and valkyrie wipe

--- config/initializers/valkyrie.rb
+++ config/initializers/valkyrie.rb
@@ -13,6 +13,14 @@ Valkyrie::StorageAdapter.register(
   :disk
 )
 
+Valkyrie::StorageAdapter.register(
+  Valkyrie::Storage::Disk.new(
+    base_path: Rails.root.join('tmp', 'files'),
+    file_mover: FileUtils.method(:cp)
+  ),
+  :test_disk
+)
+
 module Atlas
   def self.persister
     Valkyrie.config.metadata_adapter.persister

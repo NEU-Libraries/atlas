4144e68eb78b1c2b435ec4ea99e8e920aa7fc032
David Cliff <david.g.cliff@gmail.com>
2023-05-18 19:09:13 +0000

    Creation and destruction safety due to not using AR objs

--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -5,9 +5,13 @@ require 'rails_helper'
 describe CommunitiesController, type: :controller do
   render_views
 
-  let(:community) { CommunityCreator.call }
+  after :each do
+    Valkyrie.config.metadata_adapter.persister.wipe!
+  end
 
   describe 'GET #show' do
+    let(:community) { CommunityCreator.call }
+
     context 'when the community exists' do
       it 'returns the community details' do
         get :show, params: { id: community.noid }, as: :json
@@ -20,6 +24,20 @@ describe CommunitiesController, type: :controller do
   end
 
   describe 'GET #index' do
+    context 'when communities exists' do
+      it 'returns a paginated list of all communities' do
+        20.times do
+          CommunityCreator.call
+        end
+
+        get :index, as: :json
+        expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        expect(json_response['communities']).not_to be_empty
+        # TODO: make more communities
+        # TODO: ensure pagination results are correct
+      end
+    end
   end
 
   describe 'GET #mods' do
@@ -32,5 +50,15 @@ describe CommunitiesController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+    let(:community) { CommunityCreator.call }
+
+    context 'when community exists' do
+      it 'destroys the community' do
+        expect(Community.find(community.noid)).to eq(community)
+        delete :destroy, params: { id: community.noid }, as: :json
+        expect(response).to have_http_status(:success)
+        expect(Community.find(community.noid)).to be_nil
+      end
+    end
   end
 end

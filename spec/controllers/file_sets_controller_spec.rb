a4b3fc03814652bdb371c95b7c7804d0b32188a9
David Cliff <david.g.cliff@gmail.com>
2023-06-02 17:32:37 +0000

    Stubbing out controller specs

---
+++ spec/controllers/file_sets_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe FileSetsController, type: :controller do
+  render_views
+
+  after :each do
+    Valkyrie.config.metadata_adapter.persister.wipe!
+  end
+end

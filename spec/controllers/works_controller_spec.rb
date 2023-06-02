cbb4f1c9e40e2dd1ee62af9edbcffd50d614bcb7
David Cliff <david.g.cliff@gmail.com>
2023-06-02 17:41:14 +0000

    Stubbing out actions in specs

--- spec/controllers/works_controller_spec.rb
+++ spec/controllers/works_controller_spec.rb
@@ -8,4 +8,19 @@ describe WorksController, type: :controller do
   after :each do
     Valkyrie.config.metadata_adapter.persister.wipe!
   end
+
+  describe 'GET #show' do
+  end
+
+  describe 'GET #index' do
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'PATCH #update' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
 end

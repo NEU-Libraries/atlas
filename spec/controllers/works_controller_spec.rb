9646297958d450111a8fefa3d512575ca3599085
David Cliff <david.g.cliff@gmail.com>
2023-06-06 18:25:44 +0000

    Stubbing out work views

--- spec/controllers/works_controller_spec.rb
+++ spec/controllers/works_controller_spec.rb
@@ -14,7 +14,14 @@ describe WorksController, type: :controller do
     let(:collection) { CollectionCreator.call(parent_id: community.noid) }
     let(:work) { WorkCreator.call(parent_id: collection.noid) }
     it 'returns the work details' do
-      expect(work.id).not_to eq(nil)
+      title = "Test Title"
+      work.plain_title = title
+      get :show, params: { id: work.noid }, as: :json
+      expect(response).to have_http_status(:success)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['work']['id']).to eq(work.noid)
+      expect(json_response['work']['title']).to eq(title)
     end
   end
 

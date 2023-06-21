d213d70b4ca01821b0cbc1d38fc1adafa506b9bd
David Cliff <david.g.cliff@gmail.com>
2023-06-21 19:34:59 +0000

    Iterating on blobs controller spec

--- spec/controllers/blobs_controller_spec.rb
+++ spec/controllers/blobs_controller_spec.rb
@@ -5,11 +5,23 @@ require 'rails_helper'
 describe BlobsController, type: :controller do
   render_views
 
-  after :each do
-    Valkyrie.config.metadata_adapter.persister.wipe!
-  end
+  let(:community) { CommunityCreator.call }
+  let(:collection) { CollectionCreator.call(parent_id: community.noid) }
+  let(:work) { WorkCreator.call(parent_id: collection.noid) }
+  # let(:file_set) { FileSetCreator.call(work_id: work.noid, classification: Classification.generic) }
 
   describe 'GET #show' do
+    let(:blob) { BlobCreator.call(path: Rails.root.join('spec/fixtures/files/image.png').to_s, work_id: work.noid) }
+
+    context 'when the blob exists' do
+      it 'returns the blob details' do
+        get :show, params: { id: blob.noid }, as: :json
+        expect(response).to have_http_status(:success)
+
+        json_response = response.parsed_body
+        expect(json_response['blob']['id']).to eq(blob.noid)
+      end
+    end
   end
 
   describe 'GET #index' do

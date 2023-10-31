f4eda7857e1b475fe54a66e1edf2f9622a41e7d3
David Cliff <david.g.cliff@gmail.com>
2023-10-31 17:58:56 +0000

    Move solr into the compose file, wrap valkyrie initializer in Rails.application.config.to_prepare to appease zeitwerk rails 7 autloading change

--- config/initializers/valkyrie.rb
+++ config/initializers/valkyrie.rb
@@ -1,59 +1,61 @@
 # frozen_string_literal: true
 
-Valkyrie::MetadataAdapter.register(
-  Valkyrie::Persistence::Postgres::MetadataAdapter.new,
-  :postgres
-)
+Rails.application.config.to_prepare do
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Persistence::Postgres::MetadataAdapter.new,
+    :postgres
+  )
 
-Valkyrie::StorageAdapter.register(
-  Valkyrie::Storage::Disk.new(
-    base_path: Rails.root.join('tmp', 'files'),
-    file_mover: FileUtils.method(:cp)
-  ),
-  :disk
-)
-
-Valkyrie::StorageAdapter.register(
-  Valkyrie::Storage::Disk.new(
-    base_path: Rails.root.join('tmp', 'files'),
-    file_mover: FileUtils.method(:cp)
-  ),
-  :test_disk
-)
-
-Valkyrie::MetadataAdapter.register(
-  Valkyrie::Persistence::Memory::MetadataAdapter.new,
-  :memory
-)
-
-Valkyrie::MetadataAdapter.register(
-    Valkyrie::Persistence::Solr::MetadataAdapter.new(
-      connection: Blacklight.default_index.connection,
-      resource_indexer: Valkyrie::Persistence::Solr::CompositeIndexer.new(
-        Valkyrie::Indexers::AccessControlsIndexer,
-        MODSIndexer
-      )
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Disk.new(
+      base_path: Rails.root.join('tmp', 'files'),
+      file_mover: FileUtils.method(:cp)
     ),
-    :index_solr
+    :disk
+  )
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Disk.new(
+      base_path: Rails.root.join('tmp', 'files'),
+      file_mover: FileUtils.method(:cp)
+    ),
+    :test_disk
   )
 
   Valkyrie::MetadataAdapter.register(
-    Valkyrie::AdapterContainer.new(
-      persister: Valkyrie::Persistence::CompositePersister.new(
-        Valkyrie::MetadataAdapter.find(:postgres).persister,
-        Valkyrie::MetadataAdapter.find(:index_solr).persister
-      ),
-      query_service: Valkyrie::MetadataAdapter.find(:postgres).query_service
-    ),
-    :composite_persister
+    Valkyrie::Persistence::Memory::MetadataAdapter.new,
+    :memory
   )
 
-module Atlas
-  def self.persister
-    Valkyrie.config.metadata_adapter.persister
-  end
+  Valkyrie::MetadataAdapter.register(
+      Valkyrie::Persistence::Solr::MetadataAdapter.new(
+        connection:  RSolr.connect(:url => 'http://127.0.0.1:8983/solr/blacklight-core'),
+        resource_indexer: Valkyrie::Persistence::Solr::CompositeIndexer.new(
+          Valkyrie::Indexers::AccessControlsIndexer,
+          MODSIndexer
+        )
+      ),
+      :index_solr
+    )
 
-  def self.query
-    Valkyrie.config.metadata_adapter.query_service
+    Valkyrie::MetadataAdapter.register(
+      Valkyrie::AdapterContainer.new(
+        persister: Valkyrie::Persistence::CompositePersister.new(
+          Valkyrie::MetadataAdapter.find(:postgres).persister,
+          Valkyrie::MetadataAdapter.find(:index_solr).persister
+        ),
+        query_service: Valkyrie::MetadataAdapter.find(:postgres).query_service
+      ),
+      :composite_persister
+    )
+
+  module Atlas
+    def self.persister
+      Valkyrie.config.metadata_adapter.persister
+    end
+
+    def self.query
+      Valkyrie.config.metadata_adapter.query_service
+    end
   end
 end

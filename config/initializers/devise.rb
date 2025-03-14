2e47cf162f050e4a94e44c8e9eff4abc4bb5a2e7
David Cliff <dgcliff@northeastern.edu>
2025-03-14 14:55:15 +0000

    Fix for XML editor bug

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -317,6 +317,6 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = ENV['RAILS_MASTER_KEY']
   end
 end

752bff7fe5916e2656bd0a71c95a880a8ff45aa4
David Cliff <dgcliff@northeastern.edu>
2026-04-02 17:00:34 +0000

    Adding comments to delineate the distinction between fileset only functional classifications (not for ux) and model only values - the rest are derived from binaries

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -4,7 +4,7 @@
 # Blobs will have their own enumeration for display purposes
 # i.e. Download labels in UX, binary filenames etc.
 class Classification < Enumerations::Base
-  value :derivative,            name: 'Derivative'
+  value :derivative,            name: 'Derivative' # fs only
   value :map,                   name: 'Map'
   value :dataset,               name: 'Dataset'
   value :image,                 name: 'Image'
@@ -15,9 +15,9 @@ class Classification < Enumerations::Base
   value :text,                  name: 'Text'
   value :archive,               name: 'Archive'
   value :musical_notation,      name: 'Musical Notation'
-  value :descriptive_metadata,  name: 'Descriptive Metadata'
-  value :person,                name: 'Faculty and Staff'
-  value :community,             name: 'Community'
-  value :collection,            name: 'Collection'
-  value :generic,               name: 'Work'
+  value :descriptive_metadata,  name: 'Descriptive Metadata' # fs only
+  value :person,                name: 'Faculty and Staff' # model only
+  value :community,             name: 'Community' # model only
+  value :collection,            name: 'Collection' # model only
+  value :generic,               name: 'Work' # model only
 end

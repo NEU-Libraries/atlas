37db16d74ca2b06e3a77c2dbf84b1640f4bfb4c3
David Cliff <dgcliff@northeastern.edu>
2026-04-06 20:51:41 +0000

    Removing thumbnail, and adding comments for later

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,17 +2,16 @@
 
 class Label < Enumerations::Base
   # Audio
-  value :audio,               name: 'Audio File',          prefix: 'audio_'
+  value :audio,               name: 'Audio File',          prefix: 'audio_' # default for Audio classification
   value :audio_master,        name: 'Master Audio File',   prefix: 'master_'
 
   # Image
   value :image_large,         name: 'Large Image',         prefix: 'highres_'
-  value :image_master,        name: 'Master Image',        prefix: 'master_'
+  value :image_master,        name: 'Master Image',        prefix: 'master_' # default for Image classification
   value :image_medium,        name: 'Medium Image',        prefix: 'medres_'
   value :image_small,         name: 'Small Image',         prefix: 'lowres_'
-  value :image_thumbnail,     name: 'Thumbnail Image',     prefix: 'thumb_'
 
-  # Documents
+  # Documents - crosswalk with ext_check - should easily map 1:1, excluding dataset which would be manually done
   value :msexcel,             name: 'Spreadsheet',         prefix: 'excel_'
   value :mspowerpoint,        name: 'PowerPoint',          prefix: 'powerpoint_'
   value :msword,              name: 'Word Document',       prefix: 'word_doc_'
@@ -22,9 +21,9 @@ class Label < Enumerations::Base
   value :dataset,             name: 'Dataset',             prefix: 'dataset_'
 
   # Video
-  value :video,               name: 'Video File',          prefix: 'video_'
+  value :video,               name: 'Video File',          prefix: 'video_' # default for Video classification
   value :video_master,        name: 'Master Video File',   prefix: 'master_'
 
-  # Archive
+  # Archive - hm, mime_helper seems to have missed this use case. Should correlate with .zip and .tar
   value :zip,                 name: 'Zip File',            prefix: 'zipped_'
 end

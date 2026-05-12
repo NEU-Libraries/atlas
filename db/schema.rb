1c5ffa5ae7ddc05f535799ced8967e8d1897420b
David Cliff <dgcliff@northeastern.edu>
2026-05-12 17:05:05 +0000

    Capture schema.rb update from idempotency_keys migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_28_173738) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_12_164902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+
+  create_table "idempotency_keys", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "key", null: false
+    t.string "resource_type", null: false
+    t.string "resource_noid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "key"], name: "index_idempotency_keys_on_user_id_and_key", unique: true
+    t.index ["user_id"], name: "index_idempotency_keys_on_user_id"
+  end
 
   create_table "metadata_mets", force: :cascade do |t|
     t.jsonb "json_attributes"
@@ -70,4 +81,5 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_28_173738) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "idempotency_keys", "users"
 end
